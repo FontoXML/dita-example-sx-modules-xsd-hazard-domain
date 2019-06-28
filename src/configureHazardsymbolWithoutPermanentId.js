183e68fb71f91b7743f56b1ca0a22d95775399d0
eva <eva.de.haas@liones.nl>
2019-06-28 10:59:43 +0200

    Upgrade to SDK 7.7

--- src/configureHazardsymbolWithoutPermanentId.js
+++ src/configureHazardsymbolWithoutPermanentId.js
@@ -1,20 +1,14 @@
-define([
-	'fontoxml-families/configureAsImageInFrame'
-], function (
-	configureAsImageInFrame
-) {
-	'use strict';
+import configureAsImageInFrame from 'fontoxml-families/src/configureAsImageInFrame.js';
 
-	// This file removes the permanentId which is the other way around then for cross reference,
-	// for compatability reasons.
-	return function configureHazardSymbolWithoutPermanentId (sxModule) {
-		// To disable permanentId's in hazard symbols (also known as the reference pipeline), set
-		// isPermanentId to false.
+// This file removes the permanentId which is the other way around then for cross reference,
+// for compatability reasons.
+export default function configureHazardSymbolWithoutPermanentId(sxModule) {
+	// To disable permanentId's in hazard symbols (also known as the reference pipeline), set
+	// isPermanentId to false.
 
-		configureAsImageInFrame(sxModule, 'self::hazardsymbol', undefined, {
-			priority: 2,
-			referenceQuery: '@href',
-			isPermanentId: false
-		});
-	};
-});
+	configureAsImageInFrame(sxModule, 'self::hazardsymbol', undefined, {
+		priority: 2,
+		referenceQuery: '@href',
+		isPermanentId: false
+	});
+}
